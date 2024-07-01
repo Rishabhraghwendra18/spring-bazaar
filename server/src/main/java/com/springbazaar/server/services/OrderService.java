3101e693f06da6d863d11fe1af80cbb66adea518
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2024-07-01 16:00:17 +0530

    added order quantiy check before making an order and decrease in item quantity on each order

--- server/src/main/java/com/springbazaar/server/services/OrderService.java
+++ server/src/main/java/com/springbazaar/server/services/OrderService.java
@@ -3,6 +3,7 @@ package com.springbazaar.server.services;
 import com.springbazaar.server.controllers.Order;
 import com.springbazaar.server.entities.InventoryEntity;
 import com.springbazaar.server.entities.OrdersEntity;
+import com.springbazaar.server.repository.InventoryRepository;
 import com.springbazaar.server.repository.OrderRepository;
 import com.springbazaar.server.requestresponse.OrderRequest;
 import com.springbazaar.server.requestresponse.OrderWithItemIdResponse;
@@ -13,16 +14,19 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryRepository inventoryRepository;
     private final JwtUtil jwtUtil;
 
     @Autowired
-    public OrderService(OrderRepository orderRepository, JwtUtil jwtUtil) {
+    public OrderService(OrderRepository orderRepository, InventoryRepository inventoryRepository,JwtUtil jwtUtil) {
 
         this.orderRepository = orderRepository;
+        this.inventoryRepository=inventoryRepository;
         this.jwtUtil=jwtUtil;
     }
     public List<OrdersEntity> getAllBuyerOrders (String jwtToken){
@@ -31,6 +35,14 @@ public class OrderService {
         return orderRepository.findByBuyerId(userId);
     }
     public OrdersEntity createOrder(OrderRequest orderRequest, String jwtToken){
+        //        Updating Item Quantity
+        Optional<InventoryEntity> itemQuantityUpdate = inventoryRepository.findById(orderRequest.getItemId());
+        if(itemQuantityUpdate.isPresent() && itemQuantityUpdate.get().getItemQuantity() == 0) return null;
+        if (itemQuantityUpdate.isPresent() && itemQuantityUpdate.get().getItemQuantity() - 1 >= 0){
+            itemQuantityUpdate.get().setItemQuantity(itemQuantityUpdate.get().getItemQuantity()-1);
+            inventoryRepository.save(itemQuantityUpdate.get());
+        }
+
         LocalDateTime orderDate = LocalDateTime.now();
         var claims = jwtUtil.getAllClaimsFromToken(jwtToken);
         String userId = claims.getSubject();
@@ -42,6 +54,7 @@ public class OrderService {
         InventoryEntity item = new InventoryEntity();
         item.setId(orderRequest.getItemId());
         order.setItemId(item);
+
         return orderRepository.save(order);
     }
     public List<OrderWithItemIdResponse> getAllSellerOrders(String token){
