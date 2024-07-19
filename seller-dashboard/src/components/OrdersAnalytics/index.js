213eb01989f82dfd94940f40605a7767928f8143
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2024-07-19 10:04:25 +0530

    binded seller dashboard analytics api

--- seller-dashboard/src/components/OrdersAnalytics/index.js
+++ seller-dashboard/src/components/OrdersAnalytics/index.js
@@ -1,23 +1,59 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
+import { getSellerDashboardAnalytics } from "@/services/orders";
 import Card from "../Card";
 import "./index.css";
 
 function OrdersAnalytics() {
+  const [dashboardAnalytics, setDashboardAnalytics] = useState({});
+  const [loading, setLoading] = useState(false);
+  const fetchDashboardAnalytics = async () => {
+    setLoading(true);
+    try {
+      const response = await getSellerDashboardAnalytics();
+      setDashboardAnalytics(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log("error while fetching dashboard analytics data: ", error);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchDashboardAnalytics();
+  }, []);
   return (
     <div className="order-analytics-container">
       <h1>Welcome Back!</h1>
       <div className="analytics">
         <Card className={"analytics-card"}>
-            <h3>294</h3>
-            <span>Total Orders</span>
+          {loading ? (
+            <h3>Loading...</h3>
+          ) : (
+            <>
+              <h3>{dashboardAnalytics?.totalOrders}</h3>
+              <span>Total Orders</span>
+            </>
+          )}
         </Card>
         <Card className={"analytics-card"}>
-            <h3>200</h3>
-            <span>Completed Orders</span>
+          {loading ? (
+            <h3>Loading...</h3>
+          ) : (
+            <>
+              <h3>{dashboardAnalytics?.completedOrders}</h3>
+              <span>Completed Orders</span>
+            </>
+          )}
         </Card>
         <Card className={"analytics-card"}>
-            <h3>94</h3>
-            <span>New Orders</span>
+          {loading ? (
+            <h3>Loading...</h3>
+          ) : (
+            <>
+              <h3>{dashboardAnalytics?.newOrders}</h3>
+              <span>New Orders</span>
+            </>
+          )}
         </Card>
       </div>
     </div>
--- seller-dashboard/src/services/orders.js
+++ seller-dashboard/src/services/orders.js
@@ -3,5 +3,8 @@ import apiClient from "./interceptor";
 async function getSellerAllOrders() {
     return await apiClient.get("/order/getorders",{withCredentials:true});
 }
+async function getSellerDashboardAnalytics() {
+    return apiClient.get("/order/sellerdashboard",{withCredentials:true});
+}
 
-export {getSellerAllOrders}
+export {getSellerAllOrders,getSellerDashboardAnalytics}
